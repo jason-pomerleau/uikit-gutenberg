ea83be3e3f2b6b5611169b0a0b3787897aa9489f
Braeden Beaulieu <braeden@jasonpomerleau.com>
2019-07-09 15:50:52 -0400

    Added all blocks to the new repo

---
+++ src/uk-container-block/block.js
@@ -0,0 +1,104 @@
+/**
+ * BLOCK: test-block
+ *
+ * Registering a basic block with Gutenberg.
+ * Simple block, renders and saves the same content without any interactivity.
+ */
+
+//  Import CSS.
+import './style.scss';
+import './editor.scss';
+
+const { __ } = wp.i18n; // Import __() from wp.i18n
+const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
+const { InspectorControls, InnerBlocks } = wp.editor;
+const {
+    PanelBody,
+    PanelRow,
+	RadioControl
+} = wp.components;
+
+/**
+ * Register: aa Gutenberg Block.
+ *
+ * Registers a new block provided a unique name and an object defining its
+ * behavior. Once registered, the block is made editor as an option to any
+ * editor interface where blocks are implemented.
+ *
+ * @link https://wordpress.org/gutenberg/handbook/block-api/
+ * @param  {string}   name     Block name.
+ * @param  {Object}   settings Block settings.
+ * @return {?WPBlock}          The block, if it has been successfully
+ *                             registered; otherwise `undefined`.
+ */
+registerBlockType( 'cgb/uk-container-block', {
+	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
+	title: __( 'uk-container-block' ), // Block title.
+	icon: 'shield', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
+	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+	keywords: [
+		__( 'uk', 'UK' ),
+		__( 'Test' ),
+		__( 'container' ),
+	],
+	attributes: {
+		marginRadio: {
+			marginRadio: 0
+		}
+	},
+
+	/**
+	 * The edit function describes the structure of your block in the context of the editor.
+	 * This represents what the editor will render when the block is used.
+	 *
+	 * The "edit" property must be a valid function.
+	 *
+	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
+	 */
+	edit: function( props ) {
+		return [
+			//.uk-margin-medium
+			<InspectorControls>
+				<PanelBody
+					title={ __( 'Add margin?', 'uk-grid-block' ) }
+				>
+					<PanelRow>
+						<RadioControl 
+							label='Pick a margin setting'
+							options={[
+									{ label: 'None', value: '' },
+									{ label: 'Small', value: 'uk-margin-small' },
+									{ label: 'Medium', value: 'uk-margin-medium' },
+									{ label: 'Large', value: 'uk-margin-large' },
+									{ label: 'Extra Large', value: 'uk-margin-xlarge' },
+								]}
+							onChange={( value ) => {
+								props.setAttributes( { marginRadio: value } );
+							}}							
+							selected={props.attributes.marginRadio}
+						/>
+					</PanelRow>
+				</PanelBody>
+			</InspectorControls>,
+			<div className={ 'uk-container' }>
+				<InnerBlocks />
+			</div>
+		];
+	},
+
+	/**
+	 * The save function defines the way in which the different attributes should be combined
+	 * into the final markup, which is then serialized by Gutenberg into post_content.
+	 *
+	 * The "save" property must be specified and must be a valid function.
+	 *
+	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
+	 */
+	save: function( props ) {
+		return (
+			<div className={`uk-container ${props.attributes.marginRadio}`}>
+				<InnerBlocks.Content />
+			</div>
+		);
+	},
+} );
